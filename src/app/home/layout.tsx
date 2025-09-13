e4b8ede28cc6dec564f3190f6409becb1cc44989
tagorenemala <tagorenemala@gmail.com>
2025-09-13 08:23:14 +0530

    right sidebar done

--- src/app/home/layout.tsx
+++ src/app/home/layout.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import LeftSidebar from "@/components/LeftSidebar";
+import RightSidebar from "@/components/RightSidebar";
 
 export default function Layout({ children }: Readonly<{ children: React.ReactNode }>) {
     return <>
-        <div className="mr-2 md:mr-10 xl:mr-110 lg:ml-100 ml-12 min-h-screen border border-border mb-20">
-            <LeftSidebar />
+        <LeftSidebar />
+        <div className="mr-2 md:mr-10 xl:mr-110 lg:ml-100 ml-12 min-h-screen border border-border mb-20">  
+            {children}
         </div>
-        {children}
+        <RightSidebar />
+        
     </>;
 }
--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -2,6 +2,15 @@ import React from "react";
 
 export default function Page() {
     return (
-        <div></div>
+        <div>
+            <div className="border border-border h-14 grid grid-cols-2 text-white">
+                <button className="cursor-pointer font-semibold hover:bg-hover">
+                    For You
+                </button>
+                <button className="cursor-pointer font-semibold hover:bg-hover">
+                    Following
+                </button>
+            </div>
+        </div>
     )
 }
